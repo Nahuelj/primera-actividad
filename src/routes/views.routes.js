d6c2f630e441aa52b11b6a847404867c69328b42
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-10-01 23:04:50 -0300

    feat: singIn

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+import { UserModel } from "../dao/models/user.model.js";
 
 export const viewsRouter = Router();
 
@@ -22,4 +24,32 @@ viewsRouter.get("/carts/:id", (req, res) => {
     const cartId = req.params.id;
     res.setHeader("Content-Type", "text/html");
     res.render("carts", { cartId });
+});
+
+viewsRouter.get("/register", (req, res) => {
+    res.render("register");
+});
+
+viewsRouter.post("/singin", async (req, res) => {
+    const { email, password } = req.query;
+
+    if (!email || !password) {
+        return console.log("missing data");
+    }
+
+    let salt = bcrypt.genSaltSync(10);
+    const hashed_password = bcrypt.hash(password, salt);
+
+    const findUser = await UserModel.find({ email: email });
+
+    if (findUser.length === 0) {
+        const user = await UserModel.create({
+            email,
+            password: hashed_password,
+        });
+
+        return res.status(200).send("user created");
+    } else {
+        return res.send("user found");
+    }
 });
